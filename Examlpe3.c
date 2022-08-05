20980da4e5f609a850d6b4444cd6c8065bd4ced5
Esra Koc <eocesra@gmail.com>
2022-08-05 16:20:31 +0300

    It finds the number odd or even

---
+++ Examlpe3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	// A program that finds the numbers you enter as odd or even
+	
+	int number;
+	
+	printf("Please enter a number: ");
+	scanf("%d", &number);
+	
+	if (number % 2 == 0)
+		printf("%d is even!", number);
+	else
+		printf("%d is odd!", number);
+		
+	return 0;
+}
