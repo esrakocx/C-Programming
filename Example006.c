359aa3eb6d27d9b47fd4398da6e283f637b43a64
Esra Koc <eocesra@gmail.com>
2022-08-17 23:53:43 +0300

    Third while loop example

---
+++ Example006.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	// Print numbers starting from 10 in descending order
+	
+	int i = 10;
+	
+	while(i>0){
+		
+		printf("%d\n", i);
+		i--;
+	}	
+	
+	printf("Bomb!");
+	
+	return 0;
+}
