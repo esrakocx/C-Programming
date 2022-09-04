7e2a913fec547e350e0080f27d96db4b61ce1e2b
Esra Koc <eocesra@gmail.com>
2022-09-05 00:25:10 +0300

    A pyramid star triangle

---
+++ Example040.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+It prints a triangle like this (Pyramid star triangle):
+
+        *
+       * *
+      * * *
+     * * * *
+    * * * * *
+   * * * * * *
+  * * * * * * *
+ * * * * * * * * 
+* * * * * * * * *     
+
+*/
+
+int main(){
+
+	int i,j,len;
+	
+	printf("Please enter the length of the triangle: ");
+	scanf("%d", &len);
+	
+	for(i=1; i<=len; i++){
+		
+		for(j=i; j<len; j++){
+			printf(" ");
+		}
+		
+		for(j=1; j<=i; j++){
+			printf("* ");
+		}
+		
+		printf("\n");
+	}
+	
+	return 0;
+}
