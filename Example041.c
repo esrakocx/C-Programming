423dc3a889b95796cdc9f5bb848ad11ac69179d1
Esra Koc <eocesra@gmail.com>
2022-09-05 01:00:25 +0300

    An inverted pyramid star triangle

---
+++ Example041.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+It prints a triangle like this (Pyramid star triangle):
+
+ * * * * * * * * *
+  * * * * * * * *
+   * * * * * * *
+    * * * * * * 
+     * * * * *
+      * * * *
+	   * * *
+	    * *
+         *    
+
+*/
+
+int main(){
+
+	int i,j,len;
+	
+	printf("Please enter the length of the triangle: ");
+	scanf("%d", &len);
+	
+	for(i=len; i>0; i--){
+		
+		for(j=len; j>i; j--){
+			printf(" ");
+		}
+		
+		for(j=i; j>0; j--){
+			printf("* ");
+		}
+		
+		printf("\n");
+	}
+	
+	return 0;
+}
