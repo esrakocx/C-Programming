164e7d8339a7735faaa7862f3d3c2eb1c3107de0
Esra Koc <eocesra@gmail.com>
2022-09-22 22:09:25 +0300

    Find prime numbers up to the number you entered

---
+++ Example101.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include <stdlib.h>
+ 
+int IsPrime(int number, int n){
+	
+	int i, flag = 1;
+	
+	for(i=2; i<number; i++){
+		if(number%i == 0){
+			flag = 0;
+			break;
+		}
+	}
+
+	if(flag == 1)
+		return flag;
+}
+ 
+int main()
+{
+ 	int n, number;
+ 	
+ 	printf("Please enter a number to print up to that number: ");
+ 	scanf("%d", &n);
+ 	
+ 	for(number=2; number<=n; number++){
+ 		if(IsPrime(number, n) == 1)
+ 			printf("%d ", number);
+	 }
+  
+   return 0;
+}
